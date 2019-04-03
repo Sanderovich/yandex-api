a8e1316954c71b92132452d13e8240a9a1a77520
Sander Aalbers <aalbers.sander@gmail.com>
2019-04-03 20:19:58 +0200

    Create a Request Factory

---
+++ src/Requests/RequestFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Yandex\Requests;
+
+use Yandex\Exceptions\YandexException;
+
+/**
+ * Class RequestFactory
+ *
+ * @package Yandex\Requests
+ */
+class RequestFactory
+{
+    /**
+     * List of requests
+     *
+     * @var array
+     */
+    private $requests = [];
+
+    /**
+     * RequestFactory constructor.
+     *
+     * @param string $key
+     */
+    public function __construct(string $key)
+    {
+        $this->requests = [
+            'languages' => new SupportedLanguagesRequest($key),
+            'detect_language' => new DetectLanguageRequest($key),
+            'translate' => new TranslateRequest($key),
+        ];
+    }
+
+    /**
+     * Get Yandex request
+     *
+     * @param string $request
+     * @return mixed
+     * @throws YandexException
+     */
+    public function get(string $request)
+    {
+       if ($this->requests->array_key_exists($request)) {
+           return $this->requests[$request];
+       }
+
+       throw new YandexException("Request '$request' doesn't exist");
+    }
+}
