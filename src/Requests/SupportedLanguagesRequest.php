0133676eb21c4c8750a37da2c09c7252f7a38cdf
Sander Aalbers <aalbers.sander@gmail.com>
2019-04-03 20:20:07 +0200

    Add Supported Languages request

---
+++ src/Requests/SupportedLanguagesRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Yandex\Requests;
+
+/**
+ * Class SupportedLanguagesRequest
+ *
+ * @package Yandex\Requests
+ */
+class SupportedLanguagesRequest extends Request
+{
+    /**
+     * SupportedLanguagesRequest constructor.
+     *
+     * @param string $key
+     */
+    public function __construct(string $key)
+    {
+        parent::__construct($key, '/getLangs', [
+            'ui' => '',
+        ]);
+    }
+}
